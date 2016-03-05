a4306972d45f468271f7722f1afaeec125215dc9
runarberg <runarberg@zoho.com>
2016-03-05 10:58:17 +0000

    Serialize HTML form elements into JS object
    This package exports a function that inputs an HTML form element and
    outputs a java script object adhering to the name, value, and type
    properties of each element belonging to that form. The returned object
    nests following an obsolete specification[1].
    
    In addition to the old specs, we added the basic types of `number` and
    `boolean` to the outputs.
    
    * Inputs of type `checkbox` with no explicit value attribute get the
      value `true` if it is checked, and `false` otherwise.
    
    * Inputs of type `number` and `range` gets it value casted to `number`.
    
    * All other elements get their value as `string`.
    
    [1]: https://www.w3.org/TR/html-json-forms/

---
+++ README.md
@@ -0,0 +1,88 @@
+formsquare
+==========
+
+Turn your HTML5 forms into javascript objects the smart way.
+
+Formsquare is yet another [square bracket notation][1] form
+serializer, but weirder. What mainly sets formsquare appart from the
+other serializers out there is that formsquare tries to be smart about
+your form structure and keep open most possible mappings to valid JSON
+objects.
+
+The first round of wierdness comes with trying to retain the types of
+your form elements.
+
+* Inputs of type `checkbox` with no explicit value attribute get the
+  value `true` if it is checked, and `false` otherwise.
+* Inputs of type `number` and `range` gets it value casted to `number`.
+* All other elements get their value as `string`.
+
+And second round is even wierder by doing away with the traditional
+root object:
+
+* Form elements with explicit `name` attribute will nest inside an
+  object.
+* Form elements with a `name` starting with `[]` will nest inside an
+  array.
+* Form elements without an explicit `name` will either be a single
+  value (if it is the only element of the form) or an array.
+
+And if you though this couldn't get any wierder, you should see what
+formsquare does with your valued checkboxes.
+
+* A single valued checkbox will be that value if checked, otherwise it
+  will be `null`.
+* A collection (2 or more) of valued checkboxes, sharing the name,
+  will collect all checked values into an array.
+* Explicit array checkboxes will always be collected in an array if
+  checked.
+
+This means that you’ll now have more flexeble options in structuring
+your HTML forms. If your form only takes a single number, just leave
+an unamed input of type `number`:
+
+```html
+<form id="numeric-form">
+  <input type="number" value="42">
+</form>
+```
+
+```js
+let form = document.getElementById("numeric-form");
+console.log(formsquare(form));
+// 42
+
+form[0].type = "text";
+console.log(formsquare(form));
+// "42"
+```
+
+Or if your form needs a list of booleans, you only need to omit the
+value attribute:
+
+```html
+<form id="booleans-form">
+  <input type="checkbox" name="[]">
+  <input type="checkbox" name="[]" checked>
+  <input type="checkbox" name="[] value="true" checked>
+</form>
+```
+
+```js
+let form = document.getElementById("booleans-form");
+console.log(formsquare(form));
+// [false, true, "true"]
+```
+
+### Alternatives
+
+If you want a more traditional form serializer, you could take a look
+at any of these:
+
+* [form2js](https://www.npmjs.com/package/form2js)
+* [form-parse](https://www.npmjs.com/package/form-parse)
+* [form-serialize](https://www.npmjs.com/package/form-serialize)
+* [form-serializer](https://www.npmjs.com/package/form-serializer)
+* [get-form-data](https://www.npmjs.com/package/get-form-data)
+
+[1]: https://www.w3.org/TR/html-json-forms/
---
+++ src/formsquare.js
@@ -0,0 +1,188 @@
+const digitRE = /^\s*\d+\s*$/;
+
+export function formsquare(form) {
+  return Array.prototype.reduce.call(form, (obj, input) => {
+    let name = input.name;
+    let value = input.value;
+    let type = input.type;
+    let path = getPath(name);
+
+    if (type === "radio" && !input.checked) {
+      // Do nothing.
+      return obj;
+    } else if (type === "number" || type === "range") {
+      value = +value;
+    } else if (type === "checkbox" && input.getAttribute("value") === null) {
+      value = input.checked;
+    } else if (type === "checkbox" && !input.checked) {
+      if (hasArrayLeaf(path) || any(
+        (other) => other.name === name && other !== input,
+        form
+      )) {
+        return nonMember(obj, path);
+      }
+      value = null;
+    }
+
+    if (path.length === 0) {
+      if (obj === null) {
+        // A singleton value
+        return value;
+      }
+
+      if (!Array.isArray(obj)) {
+        obj = [obj];
+      }
+
+      obj.push(value);
+
+      return obj;
+    }
+
+    obj = initialize(obj, path, value);
+
+    let leaf = branch(path, obj);
+    let attr = path.slice(-1)[0];
+    let asObject = typeof attr !== "number" && any(
+      (other) => other.name.startsWith(name) &&
+        other !== input &&
+        other.name.slice(name.length).match(/\[[^\]]\]/),
+      form
+    );
+
+    setLeaf(leaf, attr, value, asObject);
+
+    return obj;
+  }, null);
+}
+
+function initialize(obj, path) {
+  if (obj === null) {
+    return typeof path[0] === "number" ?
+      [] :
+      Object.create(null);
+  }
+
+  return obj;
+}
+
+function getPath(name, path=[]) {
+  if (name.length === 0) {
+    return path;
+  }
+
+  let open = name.indexOf("[");
+  let close = name.indexOf("]");
+
+  if (open <= 0 && close === -1) {
+    path.push(name);
+
+    return path;
+  }
+
+  while (close < open && close !== -1) {
+    close = name.slice(close).indexOf("]");
+  }
+
+  if (close === -1) {
+    path.push(name);
+    return path;
+  }
+
+  if (open > 0) {
+    path.push(name.slice(0, open));
+    return getPath(name.slice(open), path);
+  }
+
+  let next = name.slice(open + 1, close);
+  let attr = next === "" ? -1 :
+      next.match(digitRE) ? +next :
+      next;
+
+  path.push(attr);
+  return getPath(name.slice(close + 1), path);
+}
+
+function branch(path, node) {
+  if (path.length <= 1) {
+    return node;
+  }
+
+  let first = path[0];
+  let rest = path.slice(1);
+  let attr = first === -1 ? node.length : first;
+  let next = node[attr];
+
+  if (typeof next === "undefined") {
+    let nextBranch = typeof rest[0] === "number" ?
+          [] :
+          Object.create(null);
+
+    if (Array.isArray(node) && attr === -1) {
+      node.push(nextBranch);
+      return branch(rest, nextBranch);
+    } else {
+      fillSparse(node, attr);
+      node[attr] = nextBranch;
+    }
+  }
+
+  return branch(rest, node[attr]);
+}
+
+function setLeaf(leaf, attr, value, asObject) {
+  attr = attr === -1 ? leaf.length : attr;
+
+  if (typeof leaf[attr] !== "undefined" && leaf[attr] !== null) {
+    if (!Array.isArray(leaf[attr])) {
+      leaf[attr] = [leaf[attr]];
+    }
+    leaf[attr].push(asObject ? {"": value} : value);
+  } else {
+    fillSparse(leaf, attr);
+    leaf[attr] = asObject ? {"": value} : value;
+  }
+}
+
+function nonMember(obj, path) {
+  // This is a non-member of an array.
+  if (obj === null) {
+    // The array hasn't been initialized yet.
+    return [];
+  }
+
+  let leaf = branch(path, obj);
+  let attr = path.slice(-1)[0];
+
+  if (path.length > 0 && typeof leaf[attr] === "undefined" && !hasArrayLeaf(path)) {
+    leaf[attr] = [];
+  }
+
+  return obj;
+}
+
+function fillSparse(node, attr) {
+  if (Array.isArray(node) && typeof attr === "number" && node.length < attr) {
+    for (let i = node.length; i < attr; i += 1) {
+      if (typeof node[i] === "undefined") {
+        node[i] = null;
+      }
+    }
+  }
+}
+
+function hasArrayLeaf(path) {
+  return typeof path.slice(-1)[0] === "number";
+}
+
+function any(p, arr) {
+  for (let el of arr) {
+    if (p(el)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+export default formsquare;
