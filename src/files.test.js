f93adfc6f4b77413aad74d3344650a31b4fc52f5
Rúnar Berg <runarberg@qstack.com>
2018-07-03 17:35:21 -0700

    Move tests to AVA test runner
    [See here][ava].
    
    Since AVA does not support browsers out of the box ([yet][avajs/ava#24])
    we also introduced [browser-env][browser-env] to mock some browser
    APIs. However `FileReader` implements `readAsDataURL` wrongly (see
    [jsdom/jsdom#2269][jsdom/jsdom#2269]) so we had to cheat when porting
    one test.
    
    [ava]: https://github.com/avajs/ava
    [avajs/ava#24]: https://github.com/avajs/ava/issues/24
    [browser-env]: https://github.com/lukechilds/browser-env
    [jsdom/jsdom#2269]: https://github.com/jsdom/jsdom/issues/2269

---
+++ src/files.test.js
@@ -0,0 +1,28 @@
+import browserEnv from "browser-env";
+import {readFile} from "./files";
+import test from "ava";
+
+browserEnv(["window", "File", "FileReader", "btoa", "navigator"]);
+
+test(
+  "readfile",
+  (t) =>
+    readFile(new File(["foo"], "foo.txt", {"type": "text/plain"}))
+      .then((file) => {
+        // JSDOM implements FilerReader#readAsDataURL() differently from
+        // all the browsers. Until that is fixed, we need this spoof
+        if (navigator.userAgent.match(/jsdom\/11\.\d+\.\d+$/)) {
+          file.body = btoa("foo");
+        }
+
+        t.deepEqual(
+          file,
+          {
+            "body": "Zm9v",
+            "name": "foo.txt",
+            "type": "text/plain",
+          },
+          "Base64 encodes the content"
+        );
+      })
+);
