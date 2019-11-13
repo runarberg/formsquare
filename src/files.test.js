08294f2b479e580fb9c4ea0e82f96d1e969f0aa3
Rúnar Berg <runarberg@qstack.com>
2019-11-13 08:39:02 -0800

    Bump browser-env from 3.2.5 to 3.3.0

--- src/files.test.js
+++ src/files.test.js
@@ -9,7 +9,7 @@ test("readfile", t =>
   readFile(new File(["foo"], "foo.txt", { type: "text/plain" })).then(file => {
     // JSDOM implements FilerReader#readAsDataURL() differently from
     // all the browsers. Until that is fixed, we need this spoof
-    if (navigator.userAgent.match(/jsdom\/11\.\d+\.\d+$/)) {
+    if (navigator.userAgent.match(/jsdom\/1[1-3]\.\d+\.\d+$/)) {
       // eslint-disable-next-line no-param-reassign
       file.body = btoa("foo");
     }
